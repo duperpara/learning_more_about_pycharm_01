b36bff9977765bbb482c36532ddd97ba56bb833f
duperpara <duperpara@gmail.com>
2021-08-10 21:45:40 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+
+class working_on_info:
+    def __init__(self, inpt, actions):
+        self.inpt = inpt
+        self.actios = actions
+        self. dict = {}
+        for action in self.actios:
+            self.dict[action] = self.gen(action)
+
+    def gen(self, var):
+        if var == "x":
+            return self.inpt + "xxx"
+        if var == "y":
+            return self.inpt + "yyy"
+        if var == "z":
+            return self.inpt + "zzz"
+
+
+if __name__ == '__main__':
+    print("Hellho World!!")
+    actions = ["x", "y", "z"]
+    obj = working_on_info("txt", actions)
+    print(obj.dict["x"])
+    print(obj.dict["y"])
+    print(obj.dict["z"])
+    #
+    # TODO: rest of the code
+    # TODO: rest of the code
+    # TODO: rest of the code
